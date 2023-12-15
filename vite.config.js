ed82953d1fc272c2c5b1deefe5e16baf50ccb3f4
stanleywodson <stanleypt@outlook.com>
2023-12-15 11:18:03 -0300

    feat: breeze + react _ ts

--- vite.config.js
+++ vite.config.js
@@ -3,25 +3,25 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-   plugins: [
-      laravel({
-         input: 'resources/js/app.jsx',
-         refresh: true,
-      }),
-      react(),
-   ],
-   watch: {
-      usePolling: true,
-      origin: 'http://localhost'
-   },
-   server: {
-      hmr: {
-         host: 'localhost'
-      },
-      host: true,
-      port: 3000,
-      watch: {
-         usePolling: true
-      }
-   }
+	plugins: [
+		laravel({
+			input: 'resources/js/app.tsx',
+			refresh: true,
+		}),
+		react(),
+	],
+	watch: {
+		usePolling: true,
+		origin: 'http://localhost'
+	},
+	server: {
+		hmr: {
+			host: 'localhost'
+		},
+		host: true,
+		port: 3000,
+		watch: {
+			usePolling: true
+		}
+	}
 });
