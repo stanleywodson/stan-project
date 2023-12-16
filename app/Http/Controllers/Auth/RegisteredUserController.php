daccea2bdaae2e69e7a57acefef3ca2951ddbdf1
stanleywodson <stanleypt@outlook.com>
2023-12-16 16:05:14 -0300

    test

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -46,7 +46,6 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
 
         // Auth::login($user);
-
         return redirect(RouteServiceProvider::HOME);
     }
 }
