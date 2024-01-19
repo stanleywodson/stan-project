785151aad295d55266dda121299140758906cf3f
stanleywodson <stanleypt@outlook.com>
2024-01-19 17:25:25 -0300

    chore:logo / config colors pallete

--- resources/js/Components/Sidebar.tsx
+++ resources/js/Components/Sidebar.tsx
@@ -8,7 +8,8 @@ export const Sidebar = () => {
     return (
         <aside className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
             <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
-                <div className="flex flex-col mt-10">
+                <div className="flex flex-col mt-6">
+
                     <div>
                         {sidebarMenus.map((item) => (
                             <div key={item.title}>
